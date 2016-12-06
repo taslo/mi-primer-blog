41019c243f96d994a2ce2f2e3f50199f02266300
taslo <maizares@mail.ru>
2016-12-06 16:42:21 -0300

    Cambie el HTML para la página.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^admin/', admin.site.urls),
+        url(r'', include('blog.urls')), 
+        url(r'^polls/', include('polls.urls', namespace="polls")),
+        url(r'^admin/', admin.site.urls),
 ]
